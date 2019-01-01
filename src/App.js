30ae92f5b57a9fe62bbaf72172cacdad9e1697c4
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2019-01-01 13:43:54 -0500

    adding starting project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        {/*<Logo />
+                <ImageLinkForm />
+                <FaceRecognation />}
+      </div>
+    );
+  }
+}
+
+export default App;
