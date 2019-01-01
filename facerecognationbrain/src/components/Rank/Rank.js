131e9228fd61979d25fb3194b7ed68933eda149b
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2019-01-01 18:48:20 -0500

    building components

---
+++ facerecognationbrain/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+				{'Ashley, your current rank is..'}
+			</div>
+			<div className='white f1'>
+				{'Ashley, your current rank is..'}
+			</div>
+		</div>
+		);
+			}
+
+export default Rank;
