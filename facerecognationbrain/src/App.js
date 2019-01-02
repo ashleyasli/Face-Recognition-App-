77c2576b49ddf4f6443ce24c7006d2fffaaa6cfe
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2019-01-01 19:13:29 -0500

    updating

--- facerecognationbrain/src/App.js
+++ facerecognationbrain/src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 300,
+      density: {
+        enable: true,
+        value_area: 1500
+                    }
+            }
+        }
+      }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Particles className='particles'
+        params={particlesOptions}
+      />
+       <Navigation />
+       <Logo /> 
+       <Rank />
+       <ImageLinkForm />       
       </div>
+     
     );
   }
 }
