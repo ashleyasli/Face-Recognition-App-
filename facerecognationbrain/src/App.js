74d87a0e4d31484a7689fe5afb49042cdfc4e292
ashleyasli <43186823+ashleyasli@users.noreply.github.com>
2019-01-28 19:34:21 -0500

    when someone signout initial the state of App.js

--- facerecognationbrain/src/App.js
+++ facerecognationbrain/src/App.js
@@ -41,11 +41,8 @@ boxs: position array of boxs that record where is the face
 route: what page is now 
 */
 
-class App extends Component {
-    constructor() {
-      super();
-      this.state = {
-        input: '',
+const initialState = {
+  input: '',
         imageUrl: '',
         boxs: [],
         route: 'signin',
@@ -57,7 +54,12 @@ class App extends Component {
           entries: 0,
           joined: ''
         }
-      }
+}
+
+class App extends Component {
+    constructor() {
+      super();
+      this.state = initialState;        
     }
 
     loadUser =(data) => {
@@ -117,6 +119,7 @@ class App extends Component {
               .then(count => {
                   this.setState(Object.assign(this.state.user, {entries: count}))
                   })
+              .catch(console.log)
           }
               this.displayFaceBox(this.calculateFaceLocation(response))
             })
@@ -125,7 +128,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout'){
-    this.setState({isSignedIn: false});
+    this.setState(initialState);
   } else if (route === 'home') {
     this.setState({isSignedIn: true});
   } 
